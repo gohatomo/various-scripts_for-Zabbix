34e370b5d7971ff91e5b538b193eff8024a81ddf
kyo <105647718+gohatomo@users.noreply.github.com>
2022-06-08 10:27:29 +0900

    z6/1.0: Added hostgroup create scripts.

---
+++ create-hostgroup.php
@@ -0,0 +1,113 @@
+#!/usr/bin/php
+<?php
+//Require
+require_once 'conf/config.php';
+require_once 'common_function.php';
+
+//Setting timezone
+date_default_timezone_set("$timezone");
+
+//Log file settings
+$date = date("Ymd");
+
+$log_file = rtrim($log_dir, '/') . "/create-hostgroup_{$date}.log";
+if (!file_exists($log_dir)) {
+	mkdir($log_dir, 0755);
+}
+
+//Datafile get
+$data_dir = rtrim($data_dir, '/') . "/create-hostgroup";
+
+foreach (glob("{$data_dir}/*.json") as $fname) {
+	if (!is_dir($fname)) {
+		$file_list[] = mb_substr(mb_strrchr("/$fname", '/', false, "$encoding"), 1, NULL, "$encoding");
+	}
+}
+if (!isset($file_list[0])) {
+	$error_message = "[ERROR] "
+		. "message:\"" . "JSON file is not exists. Check directory {$data_dir}.";
+	log_output($log_file, $error_message);
+	exit(1);
+}
+
+//API login
+$ch = curl_setup($api_url);
+$auth = api_login($api_user, $api_pass);
+
+//Display adjustment
+echo "\n";
+
+//Hostgroup create
+$count = count($file_list);
+$error_count = 0;
+for ($i = 0; $i < $count; ++$i) {
+	//Processing status display
+	processing_status_display($i+1, $count, $error_count);
+	
+	//File data get
+	$data_json = file_get_contents("{$data_dir}/$file_list[$i]");
+	$data_array = json_decode($data_json, true);
+	if (empty($data_array)) {
+		//Error count
+		$error_count = $error_count+1;
+		processing_status_display($i+1, $count, $error_count);
+		
+		//Error message output
+		$error_message = "[ERROR] "
+			. "message:\"" . "File is illegal json format." . "\", "
+			. "file:\"" . $file_list[$i] . "\"";
+		log_output($log_file, $error_message);
+		
+		//Processing skip
+		continue;
+	}
+	
+	//Hostgroup create
+	if (isset($data_array['groups'][0])) {
+		$count_groups = count($data_array['groups']);
+		for ($j = 0; $j < $count_groups; ++$j) {
+			//API Request
+			$method = 'hostgroup.create';
+			$params = array(
+				'name' => $data_array['groups'][$j]
+			);
+			$response = api_request($method, $params, $auth, '');
+			
+			if (isset($response['error'])) {
+				//Error count
+				$error_count = $error_count+1;
+				processing_status_display($i+1, $count, $error_count);
+				
+				//Error message output
+				$error_message = error_message($response);
+				log_output($log_file, $error_message);
+				
+				//Processing skip
+				continue;
+			}
+		}
+	}
+	else {
+		//Error count
+		$error_count = $error_count+1;
+		processing_status_display($i+1, $count, $error_count);
+		
+		//Error message output
+		$error_message = "[ERROR] "
+			. "message:\"" . "groups is not set." . "\", "
+			. "file:\"" . $file_list[$i] . "\"";
+		log_output($log_file, $error_message);
+		
+		//Processing skip
+		continue;
+	}
+}
+
+//Display adjustment
+echo "\n";
+
+//API logout
+api_logout($auth);
+curl_close($ch);
+
+?>
